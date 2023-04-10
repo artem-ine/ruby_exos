5a2596fef3e5e3015d9d39f3de9829bc1ae1ae07
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-04-10 17:40:09 +0200

    adding 13/14

---
+++ exo_14.rb
@@ -0,0 +1,21 @@
+name = "jean.dupont.0"
+num = 1
+email = "@email.fr"
+array = []
+
+
+num.upto(9) do |num|
+    new_mail = "#{name}#{num}#{email}"
+    array.push(new_mail)
+    num + 1
+end
+
+
+name_2 = "jean.dupont."
+two_digits = 10
+
+two_digits.upto(50) do |two_digits|
+    new_mail = "#{name_2}#{two_digits}#{email}"
+    array.push(new_mail)
+    two_digits + 1
+end
